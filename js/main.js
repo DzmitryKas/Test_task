a39f3740707b18b4cd0ddf2fe51a90e01d69be27
DzmitryKas <Kasperovich_dima@mail.ru>
2019-10-25 17:22:39 +0300

    add: add project

--- js/main.js
+++ js/main.js
@@ -1,16 +1,45 @@
-$('.owl-carousel').owlCarousel({
-  loop:true,
-  margin:10,
-  nav:true,
-  responsive:{
-      0:{
-          items:1
-      },
-      600:{
-          items:2
-      },
-      1000:{
-          items:3
-      }
-  }
-})
+const buttonMoldal = document.querySelector(".request-header");
+const buttonClose = document.querySelector(".form-close");
+const buttonNews = document.querySelector(".all-news");
+
+function show(state) {
+  document.querySelector(".modal-form").style.display = state;
+  document.querySelector(".filter").style.display = state;
+}
+
+buttonMoldal.addEventListener("click", () => {
+  show("block");
+});
+
+buttonClose.addEventListener("click", () => {
+  show("none");
+});
+
+function showNews() {
+  document.querySelectorAll(".news-hidden").forEach(el => {
+    console.log(1);
+    el.style.display = el.style.display === "block" ? "none" : "block";
+  });
+}
+
+buttonNews.addEventListener("click", () => {
+  showNews();
+});
+
+$(function() {
+  $(".dnSlide-main").dnSlide();
+});
+
+$(".dnSlide-main").dnSlide({
+  isOddShow: false,
+  width: 800,
+  height: 234,
+  dnSlideFirstWidth: 600,
+  dnSlideFirstHeight: 234,
+  autoPlay: false,
+  delay: 5000,
+  scale: 0.9,
+  speed: 500,
+  verticalAlign: "middle", // or 'bottom', 'top'
+  afterClickBtnFn: null
+});
